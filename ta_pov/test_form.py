d13b1b75f317d125a1565a8c8526f3ae7860ab12
Jim Pisano <jpisano@cisco.com>
2018-07-19 09:53:15 -0400

    First Commit

--- ta_pov/test_form.py
+++ ta_pov/test_form.py
@@ -5,53 +5,94 @@ import json
 from ta_pov import my_secrets
 
 # Smartsheet Config settings
-ss_config = dict(
-    SS_TOKEN = my_secrets.passwords["SS_TOKEN"]
-)
-
+ss_config = dict(SS_TOKEN = my_secrets.passwords["SS_TOKEN"])
 ss_token = ss_config['SS_TOKEN']
-ss = smartsheet.Smartsheet(SS_)
-sheet_name = 'US Commercial Tetration Forecast'
-customer_name = 'Advanced Disposal'
-
-# Find my Sheet ID
-response = ss.Sheets.list_sheets(include_all=True)
-response_dict = response.to_dict()
-sheets = response.data
-for sheet in sheets:
-    if sheet.name == sheet_name:
-        sheet_id = sheet.id
-
-# Get all the sheet info
-sheet = ss.Sheets.get_sheet(sheet_id,include='rowIds')
-sheet_dict = sheet.to_dict()
-
-# GET SHEET RESPONSE DEBUG CODE - DO NOT DELETE
-# print(json.dumps(sheet_dict, indent=2))
+ss = smartsheet.Smartsheet(ss_token)
 
 
-# Get all the columns from the primary sheet dict
-columns = sheet.columns
-col_id_dict = {}
-for column in columns:
-    col_id_dict[column.title] = column.id
-    if column.title == 'Customer':
-        customer_col_id = column.id
-        print(column.title, column.id)
+def get_sheet_id(sheet_name):
+    # Find Sheet ID for "sheet_name"
+    response = ss.Sheets.list_sheets(include_all=True)
+    sheets = response.data
+    for sheet in sheets:
+        if sheet.name == sheet_name:
+            sheet_id = sheet.id
 
-# Get all the rows from the primary sheet dict
-# and pull out the customer names
-rows = sheet_dict.get('rows', {})
-customer_list = []
-for row in rows:
-    cells = row['cells']
-    row_num = row['rowNumber']
-    for cell in cells:
-        if cell['columnId'] == customer_col_id:
-            customer_list.append(cell['displayValue'])
+    return(sheet_id)
 
-print(customer_list)
-print(customer_col_id)
-# print('Customer Name # '+ str(row_num), cell['displayValue'])
 
-exit()
+def get_sheet_info(sheet_id):
+    # Grab the entire sheet object from 'sheet_id'
+    sheet = ss.Sheets.get_sheet(sheet_id, include='rowIds')
+
+    # GET SHEET RESPONSE DEBUG CODE - DO NOT DELETE
+    # sheet_dict = sheet.to_dict()
+    # print(json.dumps(sheet_dict, indent=2))
+    # print(dir(sheet))
+    # print(type(sheet))
+    return(sheet)
+
+def get_col_id(sheet, col_name):
+    # Get the col id from 'sheet' object with 'col_name'
+    columns = sheet.columns
+    col_id_dict = {}
+    for column in columns:
+        col_id_dict[column.title] = column.id
+        if column.title == col_name:
+            customer_col_id = column.id
+
+    return (customer_col_id)
+
+
+def get_customer_list(sheet, customer_col_id):
+    # Get all the rows from the 'sheet' object
+    # and pull out the customer names
+
+    sheet_dict = sheet.to_dict()
+    rows = sheet_dict.get('rows', {})
+    customer_list = []
+    for row in rows:
+        cells = row['cells']
+        row_num = row['rowNumber']
+        for cell in cells:
+            if cell['columnId'] == customer_col_id:
+                customer_list.append(cell['displayValue'])
+
+    # Sort the Customer List
+    customer_list.sort()
+    return (customer_list)
+
+
+def update_pov_sheet(sheet_id, col_id, customer_list):
+    # Update the Customer Pick List from the Forecast Customers
+    column_spec = ss.models.Column({
+      'type': 'PICKLIST',
+      'options': customer_list,
+    })
+
+    # Update column & load the pick list in the Customer Name column
+    response = ss.Sheets.update_column(sheet_id, col_id, column_spec)
+    updated_column = response.result
+    return()
+
+
+if __name__ == "__main__":
+    # Get the customers from the segment forecast sheet
+    sheet_name = 'US Commercial Tetration Forecast'
+    col_name = 'Customer'
+    sheet_id = get_sheet_id(sheet_name)
+    sheet = get_sheet_info(sheet_id)
+    col_id = get_col_id(sheet, col_name)
+    customer_list = get_customer_list(sheet, col_id)
+
+    # Update the POV BOT sheet withCustomer Name pick list
+    # Using customer list from the segment forecast
+    sheet_name = 'Tetration POV On-Demand POV Status-Test'
+    col_name = 'Customer Name'
+    sheet_id = get_sheet_id(sheet_name)
+    sheet = get_sheet_info(sheet_id)
+    col_id = get_col_id(sheet, col_name)
+    update_pov_sheet(sheet_id, col_id, customer_list)
+
+
+
---
+++ ta_pov/update_request.py
@@ -0,0 +1,40 @@
+import smartsheet
+import json
+
+# Application Passwords kept here
+from ta_pov import my_secrets
+
+# Smartsheet Config settings
+ss_config = dict(
+    SS_TOKEN = my_secrets.passwords["SS_TOKEN"]
+)
+
+def update_customer_req():
+    ss_token = ss_config['SS_TOKEN']
+    ss = smartsheet.Smartsheet(ss_token)
+
+    # Configure email
+    email_spec = ss.models.MultiRowEmail()
+    email_spec.send_to = [
+      ss.models.Recipient({'email': 'jpisano@cisco.com'})]
+    #  ss.models.Recipient({'email': 'jpisano99@gmail.com'})]
+
+    email_spec.subject = 'Please update based on meeting'
+    email_spec.message = 'some message'
+    email_spec.cc_me = False
+    email_spec.row_ids = [8996155145119620]  # Second row from bottom
+    email_spec.column_ids = [4813469095618436]  # Customer Name
+    email_spec.include_attachments = False
+    email_spec.include_discussions = False
+
+    # Send update request
+    new_update_request = ss.Sheets.send_update_request(
+        550333354141572,           # sheet_id Tetration POV On-Demand POV Status-Test
+        email_spec
+    )
+
+    return
+
+
+if __name__ == "__main__":
+    update_customer_req()
